f56ea5bd90235e0eb236b16dba89466a25933679
IronZack95 <zac.carrettoni@gmail.com>
2020-05-25 17:31:58 +0200

    rendo eseguibile in maniera facile il codice

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # NeuralNetwork_HandDigits
  Matlab Neural Network on MNST Database
 
-Per eseguire il codice "main.m"
+ISTRUZIONI:
+Per eseguire il codice
+
+1) unzip "training.zip" nel folder corrente
+2) esegui in MATLAB "main.m"
--- number.m
+++ number.m
@@ -6,25 +6,25 @@ function  number(MiniBatch,yp_max)
         subplot(1,2,2)
         switch yp_max(ii)
             case 0
-            img = imread('C:\Users\Utente\Desktop\HandDigits_NeuralNetwork\numbers\zero.jpg');
+            img = imread('zero.jpg');
             case 1
-            img = imread('C:\Users\Utente\Desktop\HandDigits_NeuralNetwork\numbers\uno.jpg');
+            img = imread('uno.jpg');
             case 2
-            img = imread('C:\Users\Utente\Desktop\HandDigits_NeuralNetwork\numbers\due.jpg');
+            img = imread('due.jpg');
             case 3
-            img = imread('C:\Users\Utente\Desktop\HandDigits_NeuralNetwork\numbers\tre.jpg');
+            img = imread('tre.jpg');
             case 4
-            img = imread('C:\Users\Utente\Desktop\HandDigits_NeuralNetwork\numbers\quattro.jpg');
+            img = imread('quattro.jpg');
             case 5
-            img = imread('C:\Users\Utente\Desktop\HandDigits_NeuralNetwork\numbers\cinque.jpg');
+            img = imread('cinque.jpg');
             case 6
-            img = imread('C:\Users\Utente\Desktop\HandDigits_NeuralNetwork\numbers\sei.jpg');
+            img = imread('sei.jpg');
             case 7
-            img = imread('C:\Users\Utente\Desktop\HandDigits_NeuralNetwork\numbers\sette.jpg');
+            img = imread('sette.jpg');
             case 8
-            img = imread('C:\Users\Utente\Desktop\HandDigits_NeuralNetwork\numbers\otto.jpg');
+            img = imread('otto.jpg');
             case 9
-            img = imread('C:\Users\Utente\Desktop\HandDigits_NeuralNetwork\numbers\nove.jpg');
+            img = imread('nove.jpg');
             otherwise
         end
         imagesc(img);
